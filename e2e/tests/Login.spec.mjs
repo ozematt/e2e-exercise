0043e461a072da980ffb951ef71eb1a11659cbe2
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-14 13:00:59 +0200

    added fest test

--- e2e/tests/Login.spec.mjs
+++ e2e/tests/Login.spec.mjs
@@ -16,6 +16,11 @@ test.describe("Page loading", () => {
   test("Go to the home page", async ({ page }) => {
     await page.goto("/");
 
-    await expect(page.getByText("conduit")).toBeInTheDocument();
+    await expect(
+      page.getByText("A place to share your knowledge.")
+    ).toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "Global Feed" })
+    ).toBeVisible();
   });
 });
