6ae1bda190db17e2242aa66fbb0c6c4895a31dca
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-14 13:04:57 +0200

    added some changes

--- e2e/tests/Login.spec.mjs
+++ e2e/tests/Login.spec.mjs
@@ -1,12 +1,12 @@
 import { expect, test } from "@playwright/test";
 
-test.describe("Login flow", () => {
+test.describe("Login to the application", () => {
   test("Go to login page, submit form with valid data, verify redirection", async ({
     page,
   }) => {
     await page.goto("/");
     await page.getByRole("link", { name: "Sign in" }).click();
-    await page.getByPlaceholder("Email").fill("luke@coderslab.pl");
+    await page.getByPlaceholder("Email").fill("leia@coderslab.pl");
     await page.getByPlaceholder("Password").fill("secret");
     await page.getByRole("button").click();
     await expect(page.getByRole("link", { name: "luke" })).toBeVisible();
