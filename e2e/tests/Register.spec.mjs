9bb07d935c51983ae0967adc7524324c0c131afa
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-19 12:03:25 +0200

    added new test

---
+++ e2e/tests/Register.spec.mjs
@@ -0,0 +1,18 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Register user flow", () => {
+  test("Go to register page, submit form with valid data, verify redirection", async ({
+    page,
+  }) => {
+    const id = Date.now();
+    const email = `yoda-${id}@coderslab.pl`;
+
+    await page.goto("/");
+    await page.getByTestId("register-link").click();
+    await page.locator('input[placeholder="Your Name"]').fill("Yoda");
+    await page.locator('input[name="email"]').fill(email);
+    await page.locator('.form-control[type="password"]').fill("secret");
+    await page.getByRole("button", { name: "Sign up" }).click();
+    await expect(page.getByText("yoda")).toBeVisible();
+  });
+});
