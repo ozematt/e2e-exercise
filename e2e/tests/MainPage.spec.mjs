4a84e6b35a07317d6f645255036edb7fa53ac0ed
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-20 10:22:29 +0200

    added test

--- e2e/tests/MainPage.spec.mjs
+++ e2e/tests/MainPage.spec.mjs
@@ -12,4 +12,15 @@ test.describe("Main page element displayed", () => {
     await expect(singUp).toBeVisible();
     await expect(singIn).toBeVisible();
   });
+  test("checking log in", async ({ page }) => {
+    await page.goto("/");
+
+    await page.locator(".nav-link[href='/login']").click();
+    await page.locator("input[type='email']").fill("luke@coderslab.pl");
+    await page.locator("input[type='password']").fill("secret");
+    await page.locator("button[type='submit']").click();
+
+    const feedButton = page.getByRole("button", { name: "Your Feed" });
+    await expect(feedButton).toBeVisible();
+  });
 });
