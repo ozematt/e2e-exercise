d83f8c945788a6ee4971a19c5106ffa947b7c0cb
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-20 10:12:03 +0200

    solved one test exercise

--- e2e/tests/MainPage.spec.mjs
+++ e2e/tests/MainPage.spec.mjs
@@ -5,7 +5,11 @@ test.describe("Main page element displayed", () => {
     await page.goto("/");
 
     const feedButton = page.getByRole("button", { name: "Your Feed" });
-
     await expect(feedButton).not.toBeVisible();
+
+    const singIn = page.locator("a[href='/register']");
+    const singUp = page.locator("a[href='/login']");
+    await expect(singUp).toBeVisible();
+    await expect(singIn).toBeVisible();
   });
 });
