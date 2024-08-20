bd7936db439cd88b61d0060be5dbd3bc214de323
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-20 10:46:14 +0200

    added test

--- e2e/tests/UserActions.spec.mjs
+++ e2e/tests/UserActions.spec.mjs
@@ -7,5 +7,22 @@ test.describe("User actions on page", () => {
     const singUp = page.locator("a[href='/register']");
     await expect(singUp).toBeVisible();
     await singUp.click();
+
+    await page.locator("input[placeholder='Your Name']").fill("obi-one");
+    await page
+      .locator("input[placeholder='Email']")
+      .fill("obi-one@coderslab.pl");
+
+    await page.locator("input[placeholder='Password']").fill("123456");
+
+    await expect(page.locator("input[placeholder='Your Name']")).toHaveValue(
+      "obi-one"
+    );
+    await expect(page.locator("input[placeholder='Email']")).toHaveValue(
+      "obi-one@coderslab.pl"
+    );
+    await expect(page.locator("input[placeholder='Password']")).toHaveValue(
+      "123456"
+    );
   });
 });
