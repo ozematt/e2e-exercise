b57f5e5e347d261dea360b5c1837d7857cbc1d62
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-20 10:33:17 +0200

    added logic

--- e2e/tests/UserActions.spec.mjs
+++ e2e/tests/UserActions.spec.mjs
@@ -3,5 +3,9 @@ import { test, expect } from "@playwright/test";
 test.describe("User actions on page", () => {
   test("user flow check", async ({ page }) => {
     await page.goto("/");
+
+    const singUp = page.locator("a[href='/register']");
+    await expect(singUp).toBeVisible();
+    await singUp.click();
   });
 });
