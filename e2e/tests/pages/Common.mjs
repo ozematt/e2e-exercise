49ddffe33c122f2236bea0d226e6a6011e56459b
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-22 10:43:28 +0200

    added method in Common class

--- e2e/tests/pages/Common.mjs
+++ e2e/tests/pages/Common.mjs
@@ -5,4 +5,10 @@ class Common {
   async fillInput(name, value) {
     await this.page.locator(`input[name="${name}"]`).fill(value);
   }
+  async clearInput(name) {
+    await this.page.locator(`input[name="${name}"]`).clear();
+  }
+  async buttonClick(name) {
+    await this.page.getByRole(`"button", { name: "${name}" }`).click();
+  }
 }
