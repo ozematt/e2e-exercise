04aa19eb57e5301b0b31a0878c351ca75d2eb4ac
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-22 10:44:26 +0200

    added refactor

--- e2e/tests/pages/Common.mjs
+++ e2e/tests/pages/Common.mjs
@@ -3,12 +3,15 @@ class Common {
     this.page = page;
   }
   async fillInput(name, value) {
-    await this.page.locator(`input[name="${name}"]`).fill(value);
+    await this.#getInput.fill(value);
   }
   async clearInput(name) {
-    await this.page.locator(`input[name="${name}"]`).clear();
+    await this.#getInput.clear();
   }
   async buttonClick(name) {
     await this.page.getByRole(`"button", { name: "${name}" }`).click();
+  }
+  #getInput(name) {
+    return this.page.locator(`input[name="${name}"]`);
   }
 }
