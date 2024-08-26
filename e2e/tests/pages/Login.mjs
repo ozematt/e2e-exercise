601742382b3e95319bcb620d0cb63a49d3729a86
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-26 20:52:57 +0200

    added export const

--- e2e/tests/pages/Login.mjs
+++ e2e/tests/pages/Login.mjs
@@ -1,5 +1,7 @@
 import { Common } from "./Common.mjs";
 
+export const injectLogin = async ({ page }, use) => await use(new Login(page));
+
 class Login extends Common {
   constructor(page) {
     super(page);
