5d8998165e551e8296a08a766d1026c608c88307
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-27 15:58:39 +0200

    added edited configuration

--- testingTool/simple-flow.spec.js
+++ testingTool/simple-flow.spec.js
@@ -3,6 +3,16 @@ import { injectCommentService } from "../e2e/tests/services/Comment.mjs";
 import { injectAuthService } from "../e2e/tests/services/Auth.mjs";
 import { injectUserService } from "../e2e/tests/services/User.mjs";
 import { injectArticleService } from "../e2e/tests/services/Article.mjs";
+import { log } from "console";
+
+const CONFIGURATION = {
+  articleAuthorEmail: "author@automation.coderslab.pl",
+  articleAuthorPassword: "secret",
+  articleAuthorUsername: "Han Solo",
+  articleCommenterEmail: "commenter@automation.coderslab.pl",
+  articleCommenterPassword: "secret",
+  articleCommenterUsername: "Chewbacca",
+};
 
 const test = base.extend({
   commentService: injectCommentService,
@@ -11,48 +21,41 @@ const test = base.extend({
   articleService: injectArticleService,
 });
 
-const AUTHOR_CONFIGURATION = {
-  email: "jan2@example.com",
-  password: "secret",
-  username: "Jan",
-};
-const COMMENTING_CONFIGURATION = {
-  email: "scott@example.com",
-  password: "secret",
-  username: "Scott",
-  comment: "Good",
-};
-
-test.describe("users interaction", async () => {
-  test("test", async ({ commentService, authService, userService }) => {
+test.describe("Articles creation", async () => {
+  test("creation of two users, the author of the article, and the author of the commentary", async ({
+    commentService,
+    authService,
+    userService,
+    articleService,
+  }) => {
     const { user: createdAuthor } = await userService.create({
-      email: AUTHOR_CONFIGURATION.email,
-      password: AUTHOR_CONFIGURATION.password,
-      username: AUTHOR_CONFIGURATION.username,
+      email: CONFIGURATION.author.email,
+      password: CONFIGURATION.author.password,
+      username: CONFIGURATION.author.username,
     });
 
     const { user: createdCommenting } = await userService.create({
-      email: COMMENTING_CONFIGURATION.email,
-      password: COMMENTING_CONFIGURATION.password,
-      username: COMMENTING_CONFIGURATION.username,
+      email: CONFIGURATION.commenting.email,
+      password: CONFIGURATION.commenting.password,
+      username: CONFIGURATION.commenting.username,
     });
 
     const { user: loggedUser } = await authService.login({
-      email: AUTHOR_CONFIGURATION.email,
-      password: AUTHOR_CONFIGURATION.password,
+      email: CONFIGURATION.author.email,
+      password: CONFIGURATION.author.password,
     });
 
     const { article } = await articleService.create({
       body: "This is body for testing article e2e",
       description: "This is description for testing article e2e",
       tagList: ["#created-e2e", "#multipleTest"],
-      title: `Created by ${AUTHOR_CONFIGURATION.username}`,
+      title: `Created by ${CONFIGURATION.author.username}`,
       token: loggedUser.token,
     });
 
     const { user: loggedUser2 } = await authService.login({
-      email: COMMENTING_CONFIGURATION.email,
-      password: COMMENTING_CONFIGURATION.password,
+      email: CONFIGURATION.commenting.email,
+      password: CONFIGURATION.commenting.password,
     });
 
     // articleUrl oraz articleId wykorzystywane z odpowiedzi, którą zwraca metoda create serwisu Article
@@ -60,8 +63,21 @@ test.describe("users interaction", async () => {
       articleUrl: article.slug,
       articleId: article.id,
       authorId: createdAuthor.id,
-      comment: COMMENTING_CONFIGURATION.comment,
-      token: loggedUser.token,
+      comment: CONFIGURATION.commenting.comment,
+      token: loggedUser2.token,
+    });
+
+    console.log({
+      author: {
+        email: CONFIGURATION.author.email,
+        username: CONFIGURATION.author.username,
+        token: loggedUser.token,
+      },
+      commenting: {
+        email: CONFIGURATION.commenting.email,
+        username: CONFIGURATION.commenting.username,
+        token: loggedUser2.token,
+      },
     });
   });
 });
