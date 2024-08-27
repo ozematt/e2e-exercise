870e759269eb191c6f9d867f89fefb36de6108ba
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-27 08:38:20 +0200

    added class article

--- e2e/tests/services/Article.mjs
+++ e2e/tests/services/Article.mjs
@@ -1,8 +1,21 @@
-class Article {
+import { Common } from "./Common.mjs";
+
+export const injectArticleService = async ({ request }, use) =>
+  await use(new Article(request));
+
+class Article extends Common {
   constructor(request) {
-    this.request = request;
+    super(request);
   }
-  async create({ title, summary, description, tags }) {
-    const response = await
+  async create({ body, description, tagList, title, token }) {
+    const data = { article: { body, description, tagList, title } };
+
+    const response = await this.requestToEndpoint("/api/article", {
+      data,
+      method: "post",
+      token,
+      status: 200,
+    });
+    return response.json();
   }
 }
