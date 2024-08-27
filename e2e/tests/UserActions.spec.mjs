3ceb93311a59a0ff3659ce278497b3af00651687
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-27 09:21:02 +0200

    added logic

--- e2e/tests/UserActions.spec.mjs
+++ e2e/tests/UserActions.spec.mjs
@@ -4,18 +4,56 @@ import { injectCommon } from "./pages/Common.mjs";
 import { injectArticleService } from "./services/Article.mjs";
 import { injectAuthService } from "./services/Auth.mjs";
 import { injectCommentService } from "./services/Comment.mjs";
+import { injectUserService } from "./services/User.mjs";
 
 const test = base.extend({
   article: injectArticle,
   articleService: injectArticleService,
   commentService: injectCommentService,
   authService: injectAuthService,
-
+  userService: injectUserService,
   common: injectCommon,
 });
 
 test.describe("User actions on page", () => {
-  test.beforeEach();
+  const user = {};
+  test.beforeEach(
+    async ({ articleService, authService, commentService, userService }) => {
+      const id = Date.now();
+
+      Object.assign(user, {
+        email: `test-${id}@coderslab.pl`,
+        password: "secret",
+      });
+
+      const { user: createdUser } = await authService.create({
+        email: user.email,
+        password: user.password,
+        username: "e2euser",
+      });
+
+      const { user: loggedUser } = await authService.login({
+        email: user.email,
+        password: user.password,
+      });
+
+      const { article } = await articleService.create({
+        body: "This is body from e2e - 2",
+        description: "This is description from E2E - 2",
+        tagList: ["#createdByE2E", "#multipleTest"],
+        title: `Created by E2E - ${id}`,
+        token: loggedUser.token,
+      });
+
+      await commentService.create({
+        articleUrl: article.slug,
+        articleId: article.id,
+        authorId: createdUser.id,
+        comment: `This is a comment created in E2E by user ${user.email}`,
+        token: loggedUser.token,
+      });
+    }
+  );
   test("user flow check", async ({ page, article, common }) => {
     //entered main page
     await page.goto("http://127.0.0.1:3000");
