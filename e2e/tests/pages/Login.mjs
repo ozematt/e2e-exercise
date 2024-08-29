962be4ef160831818f4f64616a03afcdcb93ea26
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-29 15:30:17 +0200

    added logic

--- e2e/tests/pages/Login.mjs
+++ e2e/tests/pages/Login.mjs
@@ -6,6 +6,9 @@ class Login extends Common {
   constructor(page) {
     super(page);
   }
+  async fillLoginValue(name, value) {
+    await this.fillInput(name, value);
+  }
   async fillLoginInput(type, value) {
     await this.page.locator(`input[type=${type}]`).fill(value);
   }
