b3657284b24d7fe253974f76a07db620fb18d847
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-30 13:34:57 +0200

    added comments

--- e2e/tests/pages/Login.mjs
+++ e2e/tests/pages/Login.mjs
@@ -6,12 +6,15 @@ class Login extends Common {
   constructor(page) {
     super(page);
   }
+  //used class common
   async fillLoginValue(name, value) {
     await this.fillInput(name, value);
   }
+  //create new method to fill input with type use
   async fillLoginInput(type, value) {
     await this.page.locator(`input[type=${type}]`).fill(value);
   }
+  //used class common
   async click(type) {
     await this.submitClick(type);
   }
