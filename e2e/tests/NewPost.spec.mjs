5be96400411e82bd86a33114c1acd5c2c0bc0ba4
Mateusz Obirek <neutralnie7@gmail.com>
2024-08-30 13:31:18 +0200

    edited test

--- e2e/tests/NewPost.spec.mjs
+++ e2e/tests/NewPost.spec.mjs
@@ -20,27 +20,26 @@ test.describe("new post", () => {
     await newPost.fillNewPostInput("Email", "leia@coderslab.pl");
     await newPost.fillNewPostInput("Password", "secret");
     await page.getByRole("button").click();
-    await expect(page.getByRole("link", { name: "Leila" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "leia" })).toBeVisible();
 
     await newPost.clickNewPostButtons("link", "New Post");
     await newPost.fillNewPostInput("Article Title", "Sample article title");
-    await newPost.fillNewPostInput(
-      "What's this article about?",
-      "Przykładowy tekst"
-    );
+    await newPost.fillNewPostInput("What's this article about?", "Sample text");
     await newPost.fillNewPostInput(
       "Write your article (in markdown)",
-      "Przykładowy test posta"
+      "Sample post text"
     );
 
-    await newPost.fillNewPostInput("Enter tags", "Przykładowy tag");
+    await newPost.fillNewPostInput("Enter tags", "Sample tag");
     await newPost.clickNewPostButtons("button", "Publish Article");
 
     await expect(
       page.getByRole("button", { name: "Post Comment" })
     ).toBeVisible();
 
-    expect(page.url()).toBe("http://127.0.0.1:3000/article/przyk-adowy-tytu");
+    expect(page.url()).toBe(
+      "http://127.0.0.1:3000/article/sample-article-title"
+    );
 
     await expect(page.locator("h1")).toHaveText("Sample article title");
   });
