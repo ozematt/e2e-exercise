84da9aef7db4d8706c87b06a399c9a1de2a9575c
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-02 20:25:25 +0200

    added new method

--- e2e/tests/pages/Login.mjs
+++ e2e/tests/pages/Login.mjs
@@ -10,7 +10,10 @@ class Login extends Common {
   async fillLoginValue(name, value) {
     await this.fillInput(name, value);
   }
-
+  //create new method to fill input with type use
+  async fillLoginInput(type, value) {
+    await this.page.locator(`input[type=${type}]`).fill(value);
+  }
   //used class common
   async click(type) {
     await this.submitClick(type);
