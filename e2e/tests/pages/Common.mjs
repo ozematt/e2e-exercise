4ef334ca4ae16700dc70a0f3cca68d13102e2e2c
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-03 18:46:57 +0200

    added class common

--- e2e/tests/pages/Common.mjs
+++ e2e/tests/pages/Common.mjs
@@ -1,1 +1,21 @@
-
+export class Common {
+  constructor(page) {
+    this.page = page;
+  }
+  //fill input
+  async fillInput(name, value) {
+    await this.#getInput(name).fill(value);
+  }
+  //clear input
+  async clearInput(name) {
+    await this.#getInput(name).clear();
+  }
+  //click on button
+  async submitClick(type) {
+    await this.page.locator(`button[type=${type}]`).click();
+  }
+  //private method
+  #getInput(name) {
+    return this.page.locator(`input[name=${name}]`);
+  }
+}
