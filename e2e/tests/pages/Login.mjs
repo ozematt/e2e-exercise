33c7103f3f0fdcc7a404305984e9beda1b276f33
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-04 19:01:21 +0200

    added class login

--- e2e/tests/pages/Login.mjs
+++ e2e/tests/pages/Login.mjs
@@ -1,1 +1,21 @@
+import { Common } from "./Common.mjs";
 
+export const injectLogin = async ({ page }, use) => await use(new Login(page));
+
+class Login extends Common {
+  constructor(page) {
+    super(page);
+  }
+  //used class common
+  async fillLoginValue(name, value) {
+    await this.fillInput(name, value);
+  }
+  //create new method to fill input with type use
+  async fillLoginInput(type, value) {
+    await this.page.locator(`input[type=${type}]`).fill(value);
+  }
+  //used class common
+  async click(type) {
+    await this.submitClick(type);
+  }
+}
