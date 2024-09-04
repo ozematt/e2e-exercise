0b62b204dde129fc21b7c161ab09a594bc36567e
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-04 19:00:42 +0200

    added test

--- e2e/tests/Login.spec.mjs
+++ e2e/tests/Login.spec.mjs
@@ -5,5 +5,18 @@ const test = base.extend({
   login: injectLogin,
 });
 
+test.describe("Login flow", () => {
+  test("Go to login page, submit form with valid data, verify redirection @login", async ({
+    page,
+    login,
+  }) => {
+    await page.goto("/");
 
+    await page.locator(".nav-link[href='/login']").click();
+    await login.fillLoginInput("email", "luke@coderslab.pl");
+    await login.fillLoginInput("password", "secret");
+    await login.click("submit");
+
+    await expect(page.getByRole("link", { name: "luke" })).toBeVisible();
+  });
 });
