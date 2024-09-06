7f7e4f6f0bf51df4fbb5ce8f7f38233292c316e8
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-06 18:12:06 +0200

    added test

---
+++ e2e/tests/MainPage.spec.mjs
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Main page element displayed", () => {
+  test("Sing In and Sing Up element r visible @login", async ({ page }) => {
+    await page.goto("/");
+
+    const feedButton = page.getByRole("button", { name: "Your Feed" });
+    await expect(feedButton).not.toBeVisible();
+
+    const singIn = page.locator("a[href='/register']");
+    const singUp = page.locator("a[href='/login']");
+    await expect(singUp).toBeVisible();
+    await expect(singIn).toBeVisible();
+  });
+  test("checking log in @login", async ({ page }) => {
+    await page.goto("/");
+
+    await page.locator(".nav-link[href='/login']").click();
+    await page.locator("input[type='email']").fill("luke@coderslab.pl");
+    await page.locator("input[type='password']").fill("secret");
+    await page.locator("button[type='submit']").click();
+
+    const feedButton = page.getByRole("button", { name: "Your Feed" });
+    await expect(feedButton).toBeVisible();
+
+    const singIn = page.locator("a[href='/register']");
+    const singUp = page.locator("a[href='/login']");
+    await expect(singUp).not.toBeVisible();
+    await expect(singIn).not.toBeVisible();
+  });
+});
